62e8a9cd9f70beb154e97a30d2c07281fa20f336
longovalen <longo.valenn@gmail.com>
2026-07-01 08:34:08 -0400

    Add files via upload

--- results-functions.js
+++ results-functions.js
@@ -111,7 +111,7 @@ const ALL_MATCHES = [
     { id: 90, group: "R16", date: "Jul 4", team1: "Canada", team2: "Morocco" },
     { id: 91, group: "R16", date: "Jul 6", team1: "TBD", team2: "TBD" },
     { id: 92, group: "R16", date: "Jul 6", team1: "TBD", team2: "TBD" },
-    { id: 93, group: "R16", date: "Jul 5", team1: "TBD", team2: "TBD" },
+    { id: 93, group: "R16", date: "Jul 5", team1: "Mexico", team2: "TBD" },
     { id: 94, group: "R16", date: "Jul 5", team1: "Brazil", team2: "Norway" },
     { id: 95, group: "R16", date: "Jul 7", team1: "TBD", team2: "TBD" },
     { id: 96, group: "R16", date: "Jul 7", team1: "TBD", team2: "TBD" },
